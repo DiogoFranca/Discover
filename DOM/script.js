c2ab794d68459c3fd7069d53af2a2eca36b621bf
Diogo França <diogofranca845@gmail.com>
2021-05-21 10:42:21 -0300

    update files

--- DOM/script.js
+++ DOM/script.js
@@ -1,3 +1,27 @@
-let title = document.querySelector('h1')
+// getElementById (element)
 
-alert(title)
+// const element = document.getElementById('blog-title')
+// console.log(element)
+
+// getElementByClassName()(HTMLCollection)
+
+// const element = document.getElementsByClassName('blog-title')
+// console.log(element)
+
+// getElementByTagName() (HTMLCollection)
+
+// const element = document.getElementsByTagName('meta')
+// console.log(element)
+
+// querySelector() (element)
+
+// const element = document.querySelector('meta')
+// console.log(element)
+
+//querySelectorAll() (NodeList)
+
+// const element = document.querySelectorAll('[name]')
+// console.log(element)
+
+
+
